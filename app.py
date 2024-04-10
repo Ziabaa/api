eae3cb92605b2b184d74a52466c57beb7c536ad6
Ziabaa <y1vd196@gmail.com>
2024-04-10 15:31:32 +0300

    modify

--- app.py
+++ app.py
@@ -1,17 +1,46 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from PromptBilder.promt_bilder import PromptBuilder
+from PromptBilder.db import DataBase
+import pymssql
 
 app = Flask(__name__)
 
 
-# Пример обработчика для POST-запроса
-@app.route('/criteria_json', methods=['POST'])
-def process_json():
-    # Получение JSON-объекта из запроса
-    json_data = request.get_json()
-    builder = PromptBuilder()
-    prompt = builder.create_prompt_str(json_data)
-    return prompt
+@app.route('/criteria_json/<string_param>', methods=['GET', 'POST'])
+def process_json(string_param):
+    try:
+        conn = pymssql.connect(
+            host='80.73.9.240',
+            user='gonchar',
+            password='8c22T3xGpK',
+            database='Calls'
+        )
+
+        database = DataBase(conn)
+
+        if string_param == "calls":
+            check_group_value = 4
+        elif string_param == "chats":
+            check_group_value = 3
+        else:
+            return "Invalid Parameter"
+
+        query = """SELECT cl.Code, cl.Promt
+        FROM CheckGroupLists cgl
+        JOIN CheckLists cl ON cgl.CheckList = cl.id
+        WHERE cgl.CheckGroup = {}
+        FOR JSON PATH;""".format(check_group_value)
+
+        rows = database.execute_query_json(query)
+
+        res = database.formatting_to_json(rows)
+
+        conn.close()
+
+        return PromptBuilder().create_prompt_str(res)
+
+    except Exception as e:
+        return str(e), 500
 
 
 if __name__ == '__main__':
--- request.py
+++ request.py
@@ -1,14 +1,9 @@
 import requests
-import json
-
 
 # URL вашего Flask приложения
-url = 'http://127.0.0.1:5000/criteria_json'
+url = 'http://127.0.0.1:5000/criteria_json/chats'
 
-with open('criteria.json') as f:
-    data = json.load(f)
-
-response = requests.post(url, json=data)
+response = requests.post(url)
 
 # Печать ответа от сервера
 print(response.text)
